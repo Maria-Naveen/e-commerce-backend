ea46ab6f4ba7ea4da4e0c1b8514552abaca0279e
Maria Naveen <marianaveen15@outlook.com>
2024-10-17 22:56:22 +0530

    Added async error handler in controller logic

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,17 +1,18 @@
 const userService = require("../services/user.service");
+const catchAsyncError = require("../utils/catchAsyncError");
 
-const register = async (req, res) => {
+const register = catchAsyncError(async (req, res) => {
   const { name, email, password, address } = req.body;
 
   const user = await userService.register(name, email, password, address);
   res.status(201).json(user);
-};
+});
 
-const login = async (req, res) => {
+const login = catchAsyncError(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await userService.login(email, password);
   res.status(200).json(user);
-};
+});
 
 module.exports = { register, login };
--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
-  error.status = error.status || "Internal Server Error";
+  error.status = error.status || "Error";
   res.status(error.statusCode).json({
     status: error.status,
     message: error.message,
