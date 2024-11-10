86a99d1b8cfd2cd590272612a1554c700b61c9c6
Maria Naveen <marianaveen15@outlook.com>
2024-11-10 21:03:23 +0530

    Updated the global error handler

--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -1,10 +1,22 @@
 const errorHandler = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
-  error.status = error.status || "Error";
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  error.status = error.status || "error";
+
+  // Distinguish between operational (known) and programming (unknown) errors
+  if (error.isOperational) {
+    // Known errors (handled by custom error classes)
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    // Unknown errors
+    console.error("Unexpected Error: ", error); // Log error details for debugging
+    res.status(500).json({
+      status: "error",
+      message: "An unexpected error occurred.",
+    });
+  }
 };
 
 module.exports = errorHandler;
